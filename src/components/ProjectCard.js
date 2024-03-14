3471a05430df4017bd52fb5bb9cfc9421d9a430c
Valentin Virzi <v.virzi@hotmail.com>
2024-03-14 16:46:30 -0300

    projects section

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -14,14 +14,26 @@ const ProjectCard = ({ project }) => {
       </div>
       <div className="card__links">
         {/* TODO: Agregarle una etiqueta de texto a los links de Github y Open Live que precisen para qué son en lugar de que solo haya un svg */}
-        <a className="card__link" href={project.githubLink}>
+        <a
+          className="card__link"
+          href={project.githubLink}
+          target="_blank"
+          rel="noreferrer"
+        >
           <img
             className="card__link__svg"
             src="./github.svg"
             alt="GitHub Code"
           ></img>
+          <span>GitHub Code</span>
         </a>
-        <a className="card__link" href={project.deployedLink}>
+        <a
+          className="card__link"
+          href={project.deployedLink}
+          target="_blank"
+          rel="noreferrer"
+        >
+          <span>Open Deploy</span>
           <img
             className="card__link__svg"
             src="./new-tab.svg"
